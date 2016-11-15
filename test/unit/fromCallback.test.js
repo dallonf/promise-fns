b9d47c848d8ce0f3951ab4eabfb940d1f6ec8899
Dallon Feldner <dfeldner@phunware.com>
2016-11-15 10:19:15 -0600

    Another test for the error path

--- test/unit/fromCallback.test.js
+++ test/unit/fromCallback.test.js
@@ -2,11 +2,25 @@ import { assert } from 'chai';
 import fromCallback from '../../src/fromCallback';
 
 describe('fromCallback', function() {
-  it('should return a resolved promise', async function() {{
-    const hasCallback = (arg1, arg2, cb) => {
+  it('should return a resolved promise', async function() {
+    const callbackFn = (arg1, arg2, cb) => {
       cb(null, `${arg1}${arg2}`);
     };
-    const result = await fromCallback(cb => hasCallback('bat', 'man', cb));
+    const result = await fromCallback(cb => callbackFn('bat', 'man', cb));
     assert.equal(result, 'batman');
-  }});
+  });
+
+  it('should return a rejected promise', async function() {
+    const err = new Error('uh oh');
+    const callbackFn = (arg1, arg2, cb) => {
+      cb(err);
+    };
+    try {
+      const result = await fromCallback(cb => callbackFn('bat', 'man', cb));
+    } catch (caughtError) {
+      assert.equal(caughtError, err);
+      return;
+    }
+    assert(false, 'no error caught');
+  });
 });
